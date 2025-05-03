e9d00039268816cb113e83bdebed3e5963f944a9
Jon <22411356+jjhna@users.noreply.github.com>
2025-05-02 20:03:57 -0500

    testing python
    testing python in visual code studio with the command instructions

---
+++ test.py
@@ -0,0 +1,5 @@
+# To test this code in Visual Code Studio run the command in either powershell or cmd with the commands: 
+# cd D:\PythonProject\learnpython
+# python .\test.py
+
+print("Hello World")
