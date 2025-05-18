1b3d5e764cb4858f65aa084f9926b05a90768156
Jon <22411356+jjhna@users.noreply.github.com>
2025-05-18 15:09:17 -0500

    learning print statement
    learning print statements from the python institute from the 1st module

---
+++ print_statements.py
@@ -0,0 +1,21 @@
+# Learning about print statements and the many built in features for the print statements
+# cd D:\PythonProject\learnpython
+# python .\print_statements.py
+
+print("This is a the first line", "the second line will print out a blank line")
+print()
+print("this is the 3rd line \n This is the fourth line due to the new line command: \\n")
+
+print("This line here will end up together with the next line due to the empty argument ", end=" ")
+print("This is the second line that will append with the first line")
+
+print("This", "will", "put", "lines", "in", "between", sep="-")
+
+print("Programming","Essentials","in",sep="***",end="...")
+print("Python")
+
+
+print("This", "will", "add", "underscore", "line", "and", "* at the end of this line", sep="_", end="*")
+print(" This", "part", "will", "add", "*", "in", "between", "and a * and new line at the end", sep="*", end="*\n")
+print("This concludes the print statements for python")
+
